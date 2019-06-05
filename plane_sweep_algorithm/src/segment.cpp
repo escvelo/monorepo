d054aa218941f531a90e06ed89e4e3ac576aaf7f
escvelo <kulprav1986@gmail.com>
2019-06-06 01:10:40 +0200

    intersection implemented

--- plane_sweep_algorithm/src/segment.cpp
+++ plane_sweep_algorithm/src/segment.cpp
@@ -1,4 +1,6 @@
 #include<cstdio>
+#include "plane_sweep_algorithm/types.hpp"
+
 using namespace std;
 
 template <class T>
@@ -7,42 +9,80 @@ class SegmentHV {
 public:
   enum class Result{Sucess,InValid};
     
-  SegmentHV(const T& x1, const T& y1, const T& x2, const T& y2):
-    m_x1(x1), m_y1(y1), m_x2(x2), m_y2(y2) {}
+  SegmentHV(const Point2<T>& a_start, const Point2<T>& a_end):
+    m_start(a_start), m_end(a_end) {}
   bool isHorizontal() const {
-    return (m_y1 == m_y2 ? true:false);
+    return (m_start.y() == m_end.y() ? true:false);
   }
   bool isVertical() const {
-    return (m_x1 == m_x2 ? true:false);
+    return (m_start.x() == m_end.x() ? true:false);
   }
-  bool operator > (const SegmentHV<T>&  b){ // compare x coordinate
+  bool operator > (const SegmentHV<T>&  lineb) const { // compare x coordinate
                                            // if ties look for y 
-    if (m_x1 > b.m_x1){
+    if (m_start.x() > lineb.m_start.x()){
       return true;
-    } else if (m_x1 == b.m_x1 && m_y1 > b.m_y1){
+    } else if (m_start.x() == lineb.m_start.x() && m_start.y() > lineb.m_start.y()){
       return true;
     } else {
       return false;
     }
   }
 
-bool operator < (const SegmentHV<T>&  b){ // compare x coordinate
+bool operator < (const SegmentHV<T>&  lineb) const { // compare x coordinate
                                            // if ties look for y 
-    if (m_x1 < b.m_x1){
+  if (m_start.x() < lineb.start().x()){
       return true;
-    } else if (m_x1 == b.m_x1 && m_y1 < b.m_y1){
+  } else if (m_start.x() == lineb.start().x() && m_start.y() < lineb.start().y()){
       return true;
     } else {
       return false;
     }
   }
 
+bool isIntersecting(const SegmentHV<T>& segmentB) const {
+  // vector point from start to end of segmentB
+  Vector2<T> vec_segB_start_end = segmentB.end() - segmentB.start();
+  Vector2<T> vec_segB_start_segA_start = m_start - segmentB.start();
+  Vector2<T> vec_segB_start_segA_end = m_end - segmentB.start();
+
+  if (vec_segB_start_end.isLeft(vec_segB_start_segA_start) &&
+      vec_segB_start_end.isLeft(vec_segB_start_segA_end)) {
+    return false;
+  }
+  if (vec_segB_start_end.isRight(vec_segB_start_segA_start) &&
+      vec_segB_start_end.isRight(vec_segB_start_segA_end)) {
+    return false;
+  }
+
+  Vector2<T> vec_segA_start_end = m_end() - m_start;
+  Vector2<T> vec_segA_start_segB_start = segmentB.start() - m_start;
+  Vector2<T> vec_segA_start_segB_end = segmentB.start() - m_end;
+
+  if (vec_segA_start_end.isLeft(vec_segA_start_segB_start) &&
+      vec_segA_start_end.isLeft(vec_segA_start_segB_end)) {
+    return false;
+  }
+  if (vec_segA_start_end.isRight(vec_segA_start_segB_start) &&
+      vec_segA_start_end.isRight(vec_segA_start_segB_end)) {
+    return false;
+  }
+  return true;
+}
+
+Point2<T> start() const {
+    return m_start;
+  }
+Point2<T> end() const {
+    return m_end;
+  }
+  
 private:
-  T m_x1, m_y1, m_x2, m_y2;  
+  Point2<T> m_start, m_end;
   
 };
 
 
 int main(){
-  SegmentHV<int> line_segment(1,2,3,4);
+  SegmentHV<float> lineA( Point2f(1,2), Point2f(3,4) );
+  
 }
